6d8ece5ca2d4556bfba6b447aaa8f46306ee6e38
Junaid Ahmed <bhattijunaidahmed404@gmail.com>
2024-10-17 17:53:00 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Movie Search App
+
+This is a React-based Movie Search App that allows users to search for movies using the [OMDb API](http://www.omdbapi.com/). Users can search for movies by title and view information such as the movie poster, title, and release year.
+
+## Features
+
+- **Search by Title:** Search for any movie using the movie title.
+- **Search Trigger:** Search is triggered either by pressing the Enter key or by clicking the search icon.
+- **Movie Display:** The app displays a list of movies based on the search query, including movie poster, title, and release year.
+- **No Results Display:** If no movies are found, an appropriate message is shown.
+
+## Technologies Used
+
+- **React:** Front-end JavaScript library for building user interfaces.
+- **OMDb API:** Used to fetch movie data.
+- **CSS:** For styling the application.
+- **JavaScript (ES6+):** Logic and functionality of the app.
+
+## Getting Started
+
+### Prerequisites
+
+To run this project locally, you’ll need:
+
+- Node.js installed on your machine.
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/movie-search-app.git
---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=c43ec074";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  // Handle Enter key press
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      searchMovies(searchTerm);
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1>WatchU</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={handleKeyDown} // Trigger search on Enter key
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
